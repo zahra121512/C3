c7056bc68f8f717a4ce38a9975efb9c5fc8b5598
zahra121512 <jabbar1215@yahoo.com>
2024-09-18 17:13:53 +1000

    commit

---
+++ act2.py
@@ -0,0 +1,37 @@
+name = "Penguin"
+
+age = 15
+
+is_student = True 
+
+weight = 38.5 
+
+print ("Name :", name )
+
+print("Data Type of Name is", type(name))
+
+print ("Age :", age )
+
+print("Data Type of Age is", type(age))
+
+print ("is_student :",is_student )
+
+print("Data Type of is_student is", type(is_student))
+
+
+print ("Weight :", weight )
+
+print("Data Type of Weight is", type(weight))
+
+print("\n After Type Casting.....")
+
+age = str(age)
+
+print(age)
+weight = int(weight)
+
+print (weight)
+
+print("Data Type of age is", type(age))
+
+print("Data Type of Weight is", type(weight))
---
+++ act3.py
@@ -0,0 +1,7 @@
+text = str(input("Enter a string: "))
+
+revText = text[::-1]
+text =revText
+
+print("Reverse of Given  String is:")
+print(text)
